38bebd5ef2c8d1e9e65bc9830b7df28f098e994e
RmarzooqUR <marzooq697231@gmail.com>
2020-08-03 00:12:29 +0530

    some points

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,15 @@
 var flag = true     //old
 let flag1 = true
-const flag2 = null      //undefined is given by browsers when the variable is not defined - different from null 
+
+/*  undefined is given by
+    browsers when the variable
+    is not defined - different from null
+*/
+const flag2 = null
+
+
+// ------------------this is a function expression---------------
+
 document.getElementById('button').onclick = function () {
     if (flag) {
         document.getElementById('content').innerHTML = 'hello'
@@ -9,8 +18,8 @@ document.getElementById('button').onclick = function () {
         document.getElementById('content').innerHTML = ''
         flag = true
     }
-}
-
+};
+// the ; at the end just terminates the assignment statement
 
 document.getElementById('button2').onclick = function(){
     let firstName = document.getElementById('input1').value;
@@ -21,13 +30,18 @@ document.getElementById('button2').onclick = function(){
 }
 
 // array methods 
-// .join(char) - join the array and output a string with the joining character is char
+/* .join(char) - join the array and output a string
+               with the joining character is char
+                */
 // .indexOf('element') 
 // .push() | .pop()
 // .concat(['aasf','b', 20])
-// .includes() - also a string method - find the passed parameter in string or array
-
-// template strings with `the age is ${age}` - as python string formatting
+/* .includes() - also a string method - 
+                 find the passed parameter in string or array
+                */
+/* template strings with `the age is ${age}` - 
+            as python string formatting
+            */
 
 let age = 20
 list = [age, age+3, `the age is ${age}`]
@@ -39,11 +53,45 @@ let value = 200
 console.log(Number('100')+ value)     // works
 console.log(typeof value)
 
-// labels - only search for lableName above the break/continue labelName statement
-
+/* labels - only search for lableName above
+            the break/continue labelName statement
+            */
 outerlabel: for (let index = 0; index < 10; index++) {
     for (let index2 = 0; index2 < 10; index2++) {
         console.log(`index1 - ${index}, index2 ${index2}`);
         continue outerlabel;       
     }
 }
+
+// JS switch statements can be grouped and the checks are always strict
+document.getElementById('browser').oninput = function () {
+    let browser = document.getElementById('browser').value
+    switch (browser) {
+        case 'Mozilla':
+        case 'Chrome':
+        case 'Safari':
+        case 'Edge':
+            document.getElementById('result').innerHTML = 'we support this browser';
+            break;
+        case 'Internet Explorer' :
+            document.getElementById('result').innerHTML = 'this browser is not supported';
+            break;
+        default:
+            document.getElementById('result').innerHTML = 'this browser is not supported';
+            break;
+    }
+}
+
+// alert(), prompt() and confirm()
+/* alert - returns undefined | 
+    prompt - returns a string of anything
+    that was typed in the prompt
+*/
+
+
+// if same variable is declared 
+// within function block and out of it
+// the inner variable is used
+
+// we can call funcs of func exprs as a
+// parameter of another func
