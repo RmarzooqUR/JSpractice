59bc8090d16667127762d2f386b6db4eaaf83e73
RmarzooqUR <marzooq697231@gmail.com>
2020-08-02 23:03:18 +0530

    template strings | typeof

--- sandbox.js
+++ sandbox.js
@@ -10,3 +10,31 @@ document.getElementById('button').onclick = function () {
         flag = true
     }
 }
+
+
+document.getElementById('button2').onclick = function(){
+    let firstName = document.getElementById('input1').value;
+    let lastName = document.getElementById('input2').value;
+    let fullName = firstName +" "+ lastName;
+
+    document.getElementById('output').innerHTML = fullName;
+}
+
+// array methods 
+// .join(char) - join the array and output a string with the joining character is char
+// .indexOf('element') 
+// .push() | .pop()
+// .concat(['aasf','b', 20])
+// .includes() - also a string method - find the passed parameter in string or array
+
+// template strings with `the age is ${age}` - as python string formatting
+
+let age = 20
+list = [age, age+3, `the age is ${age}`]
+console.log(list)
+
+// type conversion
+// "100" + 100  - not allowed
+let value = 200
+console.log(Number('100')+ value)     // works
+console.log(typeof value)
